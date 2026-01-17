91db35fb4e6768ec5658aee4c91ec2354e8bc4d0
shadow3312 <eludeceti@gmail.com>
2026-01-17 23:22:15 +0100

    chore: add tailwind css for styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,16 +7,17 @@ import { ProfileComponent } from './features/user/profile/profile.component';
 import { RegisterComponent } from './features/auth/register/register.component';
 import { LoginComponent } from './features/auth/login/login.component';
 import { LayoutComponent } from './shared/layout/layout.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'auth/register', component: RegisterComponent },
     { path: 'auth/login', component: LoginComponent },
     {
         path: '',
         component: LayoutComponent,
         children: [
-            { path: 'dashboard', redirectTo: 'tasks', pathMatch: 'full' },
+            { path: 'dashboard', component: DashboardComponent },
             { path: 'tasks', component: MyTasksComponent },
             { path: 'tasks/new', component: TaskCreateComponent },
             { path: 'tasks/:id', component: TaskDetailComponent },
